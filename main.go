3c4944822fceafeed9823fd40f79c41ef6e406b6
Hubert Czajka <hubert.czajka@passage.com.pl>
2021-04-22 00:09:38 +0200

    ServiceMaker added

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
+	servicemaker "service-maker"
 	"time"
 )
 
@@ -14,7 +17,63 @@ func isCorrectFile(path string) bool {
 	return !fileInfo.IsDir()
 }
 
+var owkitService = servicemaker.ServiceMaker{
+	User:               "owkit",
+	UserGroups:         []string{},
+	ServicePath:        "/etc/systemd/system/owkit.service",
+	ServiceDescription: "Owkit service: reading 1-wire sensor temperature and thermostat with gpio rpi output.",
+	ExecDir:            "/srv/owkit",
+	ExecName:           "owkit",
+	ExampleConfig: `{
+		"Debug": true,
+		"RefreshSeconds": 10,
+		"Sensors": [{
+			"Name": "sensor-name",
+			"Id": 123456789,
+			"Thermostat": {
+				"Gpio": 21,
+				"Hysteresis": 0.8,
+				"Setpoint": 38,
+				"HeatUp": 8
+			}
+		}],
+		"LogInflux": {
+			"Host": "http://localhost:8086/write",
+			"Database": "dbname",
+			"Measurment": "thermostat",
+			"Tags": [{
+				"Name": "tagname",
+				"Value": "tagval"
+			}]
+		},
+		"Server": {
+			"Port": 8080,
+			"IntMultiFactor": 2
+		},
+		"OffPeak": {
+			"Url": "http://localhost:1234/offpeak"
+		},
+		"EnergyPanel": {
+			"ConnectionString": "localhost:502",
+			"HoldMinutes": 5,
+			"PowerLevel": 1000
+		}
+	}`,
+}
+
 func main() {
+	flagInstall := flag.Bool("install", false, "Install service in os")
+	flag.Parse()
+	if *flagInstall {
+		err := owkitService.InstallService()
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Println("service installed!")
+			return
+		}
+	}
+
 	var err error
 	var path string
 
